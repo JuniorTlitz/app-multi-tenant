2b7624672bbc18927d4a9d9b32f0b5a19ebbead4
Tarcizio Junior <tarciziosantos.dev@gmail.com>
2025-09-24 12:51:29 -0300

    feat: add signin route to the authentication controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { FastifyPluginAsync } from "fastify";
 import { SignUpController } from "../controllers/auth/SignUpController";
+import { SignInController } from "../controllers/auth/SignInController";
 
 export const routes: FastifyPluginAsync = async (fastify) => {
   fastify.post("/auth/signup", SignUpController.handle);
+  fastify.post("/auth/signin", SignInController.handle);
 };
