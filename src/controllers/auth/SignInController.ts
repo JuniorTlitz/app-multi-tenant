f00e7f950a3642c296ed24d7a1afe7c4aa57f25a
Tarcizio Junior <tarciziosantos.dev@gmail.com>
2025-09-24 16:06:51 -0300

    feat: enhance SignInController to include organization details in access token

--- src/controllers/auth/SignInController.ts
+++ src/controllers/auth/SignInController.ts
@@ -14,14 +14,24 @@ export class SignInController {
 
     const user = await db.user.findUnique({
       where: { email },
-      select: { id: true, password: true },
+      select: { id: true, password: true, organizations: true },
     });
 
     if (!user || !(await compare(password, user.password))) {
       return reply.code(401).send({ error: "Invalid credentials" });
     }
 
-    const accessToken = request.server.jwt.sign({ sub: user.id });
+    const [organization] = user.organizations;
+
+    if (!organization) {
+      return reply.code(403).send({ error: "You dont have any organization" });
+    }
+
+    const accessToken = request.server.jwt.sign({
+      sub: user.id,
+      organizationId: organization.orgnanizationId,
+      role: organization.role,
+    });
 
     reply.code(201).send({ accessToken });
   }
