5a40f93721b045600362536e9a908d884ebb0fdd
Tarcizio Junior <tarciziosantos.dev@gmail.com>
2025-09-24 16:07:02 -0300

    fix: correct organizationId retrieval in ListLeadsController and fix typo in query

--- src/controllers/leads/ListLeadsController.ts
+++ src/controllers/leads/ListLeadsController.ts
@@ -1,14 +1,15 @@
-import { randomUUID } from "crypto";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { db } from "../../lib/db";
 
 export class ListLeadsController {
   static async handle(request: FastifyRequest, reply: FastifyReply) {
-    const organizationId = randomUUID();
+    const { organizationId } = request.user;
+
+    console.log("organizationId", request.user);
 
     const leads = await db.lead.findMany({
       where: {
-        orgnanizationId: organizationId,
+        organizationId,
       },
     });
 
