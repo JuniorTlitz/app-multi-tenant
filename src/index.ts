8573cc1a25480b061709c8958fa0acafc9f0e214
Tarcizio Junior <tarciziosantos.dev@gmail.com>
2025-09-24 12:59:36 -0300

    feat: register fastify-jwt for authentication with JWT secret and expiration

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import Fastify from "fastify";
 import { routes } from "./routes";
 import { ZodError } from "zod";
+import fastifyJwt from "@fastify/jwt";
 
 const fastify = Fastify();
 
 fastify.register(routes);
+fastify.register(fastifyJwt, {
+  secret: process.env.JWT_SECRET!,
+  sign: {
+    expiresIn: "1h",
+  },
+});
 
 fastify.setErrorHandler((error, request, reply) => {
   if (error instanceof ZodError) {
