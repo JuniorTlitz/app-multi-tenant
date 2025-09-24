861f13f6faf94d8b494eb90a8244506aa8f35cfe
Tarcizio Junior <tarciziosantos.dev@gmail.com>
2025-09-24 15:07:40 -0300

    feat: update ListLeadsController to retrieve leads based on organization ID

--- src/controllers/leads/ListLeadsController.ts
+++ src/controllers/leads/ListLeadsController.ts
@@ -1,9 +1,19 @@
+import { randomUUID } from "crypto";
 import { FastifyReply, FastifyRequest } from "fastify";
+import { db } from "../../lib/db";
 
 export class ListLeadsController {
   static async handle(request: FastifyRequest, reply: FastifyReply) {
+    const organizationId = randomUUID();
+
+    const leads = await db.lead.findMany({
+      where: {
+        orgnanizationId: organizationId,
+      },
+    });
+
     return {
-      leads: [],
+      leads,
     };
   }
 }
